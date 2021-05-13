e482fb576103534f1ec8b429f1ac08f44813ef59
Divyanshi Verma <divyanshiv@cybage.com>
2021-05-13 11:05:54 +0530

    fetching record in audio panel from db json -server

--- js_audio/script1.js
+++ js_audio/script1.js
@@ -1,104 +1,136 @@
-$(document).ready(function(){
-    var player = $('.player'),
-    audio = player.find('audio'),
-    duration = $('.duration'),
-    currentTime = $('.current-time'),
-    progressBar = $('.progress span'),
-    mouseDown = false,
-    rewind, showCurrentTime;
+
+$(document).ready(function () {
+  $("body").on("load", function () {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:3000/songs/1",
+      dataType: "json",
+      async: true,
+      success: function (data) {
+        console.log(data);
+        let products = "";
+        let s="";
+        // $.each(data, function (i, v) {
+          products += `
+
+         
+            <div><img src="${data.image}" alt="3rdburglar by Wordburglar" /></div> `
+     
+          ;
+        // });
+        $(".cover").append(products);
+        $('audio').append(`<source src="${data.title}" type="audio/ogg" />`);
+        // $(".audio1").append(s);
+       
+      },
+      error: function () {
+        console.log("not able to process request");
+      },
+    });
+  });
+  $("body").trigger("load");
+  
+  var player = $('.player'),
+  audio = player.find('audio'),
+  duration = $('.duration'),
+  currentTime = $('.current-time'),
+  progressBar = $('.progress span'),
+  mouseDown = false,
+  rewind, showCurrentTime;
 
 function secsToMins(time) {
-  var int = Math.floor(time),
-      mins = Math.floor(int / 60),
-      secs = int % 60,
-      newTime = mins + ':' + ('0' + secs).slice(-2);
-  
-  return newTime;
+var int = Math.floor(time),
+    mins = Math.floor(int / 60),
+    secs = int % 60,
+    newTime = mins + ':' + ('0' + secs).slice(-2);
+
+return newTime;
 }
 
 function getCurrentTime() {
-  var currentTimeFormatted = secsToMins(audio[0].currentTime),
-      currentTimePercentage = audio[0].currentTime / audio[0].duration * 100;
-  
-  currentTime.text(currentTimeFormatted);
-  progressBar.css('width', currentTimePercentage + '%');
-  
-  if (player.hasClass('playing')) {
-    showCurrentTime = requestAnimationFrame(getCurrentTime);
-  } else {
-    cancelAnimationFrame(showCurrentTime);
-  }
+var currentTimeFormatted = secsToMins(audio[0].currentTime),
+    currentTimePercentage = audio[0].currentTime / audio[0].duration * 100;
+
+currentTime.text(currentTimeFormatted);
+progressBar.css('width', currentTimePercentage + '%');
+
+if (player.hasClass('playing')) {
+  showCurrentTime = requestAnimationFrame(getCurrentTime);
+} else {
+  cancelAnimationFrame(showCurrentTime);
+}
 }
 
 audio.on('loadedmetadata', function() {
-  var durationFormatted = secsToMins(audio[0].duration);
-  duration.text(durationFormatted);
+var durationFormatted = secsToMins(audio[0].duration);
+duration.text(durationFormatted);
 }).on('ended', function() {
-  if ($('.repeat').hasClass('active')) {
-    audio[0].currentTime = 0;
-    audio[0].play();
-  } else {
-    player.removeClass('playing').addClass('paused');
-    audio[0].currentTime = 0;
-  }
+if ($('.repeat').hasClass('active')) {
+  audio[0].currentTime = 0;
+  audio[0].play();
+} else {
+  player.removeClass('playing').addClass('paused');
+  audio[0].currentTime = 0;
+}
 });
 
 $('button').on('click', function() {
-  var self = $(this);
-  
-  if (self.hasClass('play-pause') && player.hasClass('paused')) {
-    player.removeClass('paused').addClass('playing');
-    audio[0].play();
-    getCurrentTime();
-  } else if (self.hasClass('play-pause') && player.hasClass('playing')) {
-    player.removeClass('playing').addClass('paused');
-    audio[0].pause();
-  }
-  
-  if (self.hasClass('shuffle') || self.hasClass('repeat')) {
-    self.toggleClass('active');
-  }
+var self = $(this);
+
+if (self.hasClass('play-pause') && player.hasClass('paused')) {
+  player.removeClass('paused').addClass('playing');
+  audio[0].play();
+  getCurrentTime();
+} else if (self.hasClass('play-pause') && player.hasClass('playing')) {
+  player.removeClass('playing').addClass('paused');
+  audio[0].pause();
+}
+
+if (self.hasClass('shuffle') || self.hasClass('repeat')) {
+  self.toggleClass('active');
+}
 }).on('mousedown', function() {
-  var self = $(this);
-  
-  if (self.hasClass('ff')) {
-    player.addClass('ffing');
-    audio[0].playbackRate = 2;
-  }
-  
-  if (self.hasClass('rw')) {
-    player.addClass('rwing');
-    rewind = setInterval(function() { audio[0].currentTime -= .3; }, 100);
-  }
+var self = $(this);
+
+if (self.hasClass('ff')) {
+  player.addClass('ffing');
+  audio[0].playbackRate = 2;
+}
+
+if (self.hasClass('rw')) {
+  player.addClass('rwing');
+  rewind = setInterval(function() { audio[0].currentTime -= .3; }, 100);
+}
 }).on('mouseup', function() {
-  var self = $(this);
-  
-  if (self.hasClass('ff')) {
-    player.removeClass('ffing');
-    audio[0].playbackRate = 1;
-  }
-  
-  if (self.hasClass('rw')) {
-    player.removeClass('rwing');
-    clearInterval(rewind);
-  }
+var self = $(this);
+
+if (self.hasClass('ff')) {
+  player.removeClass('ffing');
+  audio[0].playbackRate = 1;
+}
+
+if (self.hasClass('rw')) {
+  player.removeClass('rwing');
+  clearInterval(rewind);
+}
 });
 
 player.on('mousedown mouseup', function() {
-  mouseDown = !mouseDown;
+mouseDown = !mouseDown;
 });
 
 progressBar.parent().on('click mousemove', function(e) {
-  var self = $(this),
-      totalWidth = self.width(),
-      offsetX = e.offsetX,
-      offsetPercentage = offsetX / totalWidth;
-  
-  if (mouseDown || e.type === 'click') {
-    audio[0].currentTime = audio[0].duration * offsetPercentage;
-    if (player.hasClass('paused')) {
-      progressBar.css('width', offsetPercentage * 100 + '%');
-    }
+var self = $(this),
+    totalWidth = self.width(),
+    offsetX = e.offsetX,
+    offsetPercentage = offsetX / totalWidth;
+
+if (mouseDown || e.type === 'click') {
+  audio[0].currentTime = audio[0].duration * offsetPercentage;
+  if (player.hasClass('paused')) {
+    progressBar.css('width', offsetPercentage * 100 + '%');
   }
+}
 });
-})
+
+});
