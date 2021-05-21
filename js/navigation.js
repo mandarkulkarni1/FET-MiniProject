6d10570759ae9068deaede9c1c889d92643c4bb1
Mandar Uday Kulkarni <mandarkul@cybage.com>
2021-05-22 03:08:50 +0530

    Changed Profile and profile settings page UI and overall UX overhaul

--- js/navigation.js
+++ js/navigation.js
@@ -4,12 +4,7 @@ $(document).ready(function () {
     $('#userutton').click(function () {
       window.location.href = "../ui/profile.html"
     })
-  
-    //Tooltip activator
-    $(document).ready(function () {
-      $('[data-toggle="tooltip"]').tooltip();
-    });
-  
+    
     //function to logout
     $('#logoutbutton').click(function () {
       sessionStorage.clear();
