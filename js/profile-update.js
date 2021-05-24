2f6d61a530fcc6800eb5e530ed6b6f13fcb43d45
MaheshShingane <maheshshing@cybage.com>
2021-05-24 08:46:32 +0530

    some changes in profile update

--- js/profile-update.js
+++ js/profile-update.js
@@ -20,25 +20,10 @@ $(document).ready(function () {
         $("#ephone").attr("value", `${data.phone}`);
         $("#eemail").attr("value", `${data.id}`);
 
-        //loading profile img on profile page
-
-        if (data.profilePic === "") {
-          $(".avatar").css({
-            "background-color": "gray",
-            width: "200px",
-            height: "200px",
-            "border-radius": "50%",
-          });
-        } else {
-          $(".avatar").append(`
-              <img  src="${data.profilePic}" alt="Circle Image"
-              class="img-raised rounded-circle img-fluid my-img">                
-          `);
-        }
         //loading profile img on edit-profile page
 
         $(".my-pic").append(`<img class="profile-pic"
-     style="width: 140px; height: 140px; border-radius: 20px;" src="${data.profilePic}" alt=""> `);
+           style="width: 140px; height: 140px; border-radius: 20px;" src="${data.profilePic}" alt=""> `);
       },
       error: function () {
         console.log("not able to process request");
@@ -50,7 +35,7 @@ $(document).ready(function () {
   /* change profile pic */
 
   //warnig about img size
-  $(".upload-button").hover(
+  $(".file-upload").hover(
     function () {
       $(".img-warning").css("visibility", "visible");
     },
@@ -69,7 +54,6 @@ $(document).ready(function () {
       dataType: "json",
       async: true,
       success: function (data) {
-
         data = JSON.parse(
           JSON.stringify(data).replace("recentlyPlayed[]", "recentlyPlayed")
         );
@@ -80,7 +64,7 @@ $(document).ready(function () {
           phone: data.phone,
           password: data.password,
           profilePic: `../assets/images/profile/${pic}`,
-          recentlyPlayed: data.recentlyPlayed
+          recentlyPlayed: data.recentlyPlayed,
         };
 
         //  call for updating profile picture
@@ -101,11 +85,6 @@ $(document).ready(function () {
     filePath(this);
   });
 
-  //call for file-upload on click of button
-  $(".upload-button").on("click", function () {
-    $(".file-upload").click();
-  });
-
   // edit profilr data
 
   $("#submitData").on("click", function (e) {
@@ -124,10 +103,6 @@ $(document).ready(function () {
     var curPassword = $("#ecurPassword").val();
     var newPassword = $("#enewPassword").val();
     var cnfPassword = $("#ecnfPassword").val();
-    if (newPassword.length < 8 && cnfPassword.length < 8) {
-      $("#dialogPass").show().fadeOut(2000);
-      return;
-    }
 
     //for getting profile pic
     $.ajax({
@@ -136,7 +111,6 @@ $(document).ready(function () {
       dataType: "json",
       async: true,
       success: function (data) {
-
         data = JSON.parse(
           JSON.stringify(data).replace("recentlyPlayed[]", "recentlyPlayed")
         );
@@ -148,7 +122,7 @@ $(document).ready(function () {
             phone: phone,
             password: data.password,
             profilePic: data.profilePic,
-            recentlyPlayed: data.recentlyPlayed
+            recentlyPlayed: data.recentlyPlayed,
           };
           //AJAX Request to update data to rest API
 
@@ -161,6 +135,10 @@ $(document).ready(function () {
             },
           });
         } else {
+          if (newPassword.length < 8 && cnfPassword.length < 8) {
+            $("#dialogPass").show().fadeOut(2000);
+            return;
+          }
           //Password Confirmation
           if (curPassword !== data.password) {
             alert("Current Passwords Don't Match");
@@ -182,7 +160,7 @@ $(document).ready(function () {
             phone: phone,
             password: cnfPassword,
             profilePic: data.profilePic,
-            recentlyPlayed: data.recentlyPlayed
+            recentlyPlayed: data.recentlyPlayed,
           };
           //AJAX Request to update data to rest API
 
@@ -198,5 +176,4 @@ $(document).ready(function () {
       },
     });
   });
-
 });
